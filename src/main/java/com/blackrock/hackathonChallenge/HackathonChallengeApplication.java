e2c06438d92345b20c4ee0592a173aae51caef80
Mani <kmanikanta81311@gmail.com>
2026-02-21 14:28:13 +0530

    initial commit

---
+++ src/main/java/com/blackrock/hackathonChallenge/HackathonChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.blackrock.hackathonChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blackrock/hackathonChallenge/HackathonChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackrock.hackathonChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
